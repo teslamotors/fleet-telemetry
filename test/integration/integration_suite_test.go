2a18c252dbde79177b200c37d17e0353a5ada425
emirot <emirot.nolan@gmail.com>
2025-02-23 22:27:43 -0800

    chore: upgrade ginkgo

--- test/integration/integration_suite_test.go
+++ test/integration/integration_suite_test.go
@@ -1,16 +1,14 @@
 package integration_test
 
 import (
-	"testing"
-	"time"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"testing"
 
 	types "github.com/onsi/ginkgo/v2/types"
 )
 
 func TestTelemetry(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Integration Suite Tests", types.ReporterConfig{SlowSpecThreshold: 10 * time.Second})
+	RunSpecs(t, "Integration Suite Tests", types.ReporterConfig{})
 }
